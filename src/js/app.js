4c70b3ae41b4439a8d7a3ef41343d7fef59779a9
Stefani Ilieva <stefani.ilieva@boom.dev>
2022-02-15 16:44:10 +0200

    feat: add unstyled pricing

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,4 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
 });
